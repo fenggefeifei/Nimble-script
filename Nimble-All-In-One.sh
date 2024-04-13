ed510807d59d3df0b90a6343e93ac44ed6b05fd4
Overlode <36828226+neo250376@users.noreply.github.com>
2024-04-13 22:28:15 +0100

    Add files via upload
    Added extra error detection and more informative messages

--- Nimble-All-In-One.sh
+++ Nimble-All-In-One.sh
@@ -5,43 +5,56 @@ green='\033[0;32m'
 red='\033[0;31m'
 clear='\033[0m'
 
-# Install dialog and update system
-echo -e "${green}Updating system with apt-get${clear}"
-sudo apt-get update > /dev/null
-sudo apt-get install dialog tmux build-essential make python3-venv -y > /dev/null
-sudo rm -rf .nimble-network > /dev/null
-
 # Function to check if Go version 1.22 is installed
-
 check_go_version() {
     if ! command -v go &> /dev/null; then
         echo -e "${green}Go is not installed. Installing Go version 1.22...${clear}"
-        sudo snap install go --classic >/dev/null
+        sudo snap install go --classic >/dev/null || { echo -e "${red}Failed to install Go. Returning to options menu.${clear}"; sleep 3; return; }
         echo -e "${green}Go version 1.22 has been installed.${clear}"
         sleep 3
-    elif [[ "$(go version)" == *"1.22"* ]]; then
+    elif [[ "$(go version)" != *"1.22"* ]]; then
+        echo -e "${green}Updating Go to version 1.22...${clear}"
+        sudo snap refresh go --channel=1.22 >/dev/null || { echo -e "${red}Failed to update Go. Returning to options menu.${clear}"; sleep 3; return; }
+        echo -e "${green}Go has been updated to version 1.22.${clear}"
+        sleep 3
+    else
         echo -e "${green}Go version 1.22 is already installed.${clear}"
         sleep 3
     fi
 }
 
-# Call function to check and install Go version
+# Function to install NVIDIA driver 545
+install_nvidia_driver() {
+    if ! dpkg -l | grep -q "nvidia-driver-545"; then
+        echo -e "${green}Installing NVIDIA driver 545... please wait.${clear}"
+        sudo apt-get install nvidia-driver-545 -y >/dev/null || { echo -e "${red}Failed to install NVIDIA driver. Returning to options menu.${clear}"; sleep 3; return; }
+        sleep 3  # Adding a delay before returning to the options menu
+    else
+        echo -e "${green}NVIDIA driver 545 is already installed.${clear}"
+        sleep 3  # Adding a delay before returning to the options menu
+    fi
+}
+
+# Function to install NVIDIA CUDA Toolkit
+install_cuda_toolkit() {
+    if ! dpkg -l | grep -q "nvidia-cuda-toolkit"; then
+        echo -e "${green}Installing NVIDIA CUDA Toolkit... This may take some time depending on your download speed.${clear}"
+        sudo apt-get install nvidia-cuda-toolkit -y >/dev/null || { echo -e "${red}Failed to install NVIDIA CUDA Toolkit. Returning to options menu.${clear}"; sleep 3; return; }
+        sleep 3  # Adding a delay before returning to the options menu
+    else
+        echo -e "${green}NVIDIA CUDA Toolkit is already installed.${clear}"
+        sleep 3  # Adding a delay before returning to the options menu
+    fi
+}
+
+# Call function to check and install/update Go version
 check_go_version
-
-# Install NVIDIA driver if not installed
-if ! dpkg -l | grep -q "nvidia-driver-545"; then
-    echo -e "${green}Installing NVIDIA driver 545...please wait...${clear}"
-    sudo apt-get install nvidia-driver-545 -y > /dev/null
-fi
-
-# Install NVIDIA CUDA Toolkit if not installed
-if ! dpkg -l | grep -q "nvidia-cuda-toolkit"; then
-    echo -e "${green}Installing NVIDIA CUDA Toolkit...This can take some time depending on your download speed!${clear}"
-    sudo apt-get install nvidia-cuda-toolkit -y > /dev/null
-fi
+# Call function to install NVIDIA driver
+install_nvidia_driver
+# Call function to install NVIDIA CUDA Toolkit
+install_cuda_toolkit
 
 # Function to install the wallet
-
 install_wallet() {
     # Check if the wallet has already been installed
     if [ ! -f "$HOME/go/bin/nimble-networkd" ]; then
@@ -49,17 +62,17 @@ install_wallet() {
         # Remove the directory if it exists
         if [ -d "$HOME/nimble/wallet-public" ]; then
             echo -e "${green}Removing existing wallet-public directory...${clear}"
-            rm -rf "$HOME/nimble/wallet-public"
+            rm -rf "$HOME/nimble/wallet-public" || { echo -e "${red}Failed to remove existing directory. Returning to options menu.${clear}"; sleep 3; return; }
         fi
         # Git clone into wallet and compile
-        mkdir -p "$HOME/nimble" && cd "$HOME/nimble" || exit
-        git clone https://github.com/nimble-technology/wallet-public.git > /dev/null || exit
-        cd wallet-public || exit
-        make install || exit
+        mkdir -p "$HOME/nimble" && cd "$HOME/nimble" || { echo -e "${red}Failed to create directory. Returning to options menu.${clear}"; sleep 3; return; }
+        git clone https://github.com/nimble-technology/wallet-public.git > /dev/null || { echo -e "${red}Failed to clone repository. Returning to options menu.${clear}"; sleep 3; return; }
+        cd wallet-public || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
+        make install || { echo -e "${red}Failed to install wallet. Returning to options menu.${clear}"; sleep 3; return; }
     fi
 
     # Ask user to enter sub-wallet name
-    cd "$HOME/go/bin" || exit
+    cd "$HOME/go/bin" || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
     echo -e "${green}Please enter a name for your wallet...${clear}"
     read -r varwallet
     ./nimble-networkd keys add "$varwallet"
@@ -72,22 +85,22 @@ install_wallet() {
         sleep 3
     else
         echo -e "${red}Please record your wallet seed phrase and address before continuing.${clear}"
-        exit 1
+        sleep 3
+        return
     fi
 }
 
 # Function to install the miner
-
 install_miner() {
     # Check if the miner has already been installed
     if [ ! -f "$HOME/go/bin/nimble-miner" ]; then
         echo -e "${green}Cloning into miner and compiling...${clear}"
         # Git clone into miner and compile
-        mkdir -p "$HOME/nimble" && cd "$HOME/nimble" || exit
-        git clone https://github.com/nimble-technology/nimble-miner-public.git > /dev/null || exit
-        cd nimble-miner-public || exit
-        git pull || exit
-        make install || exit
+        mkdir -p "$HOME/nimble" && cd "$HOME/nimble" || { echo -e "${red}Failed to create directory. Returning to options menu.${clear}"; sleep 3; return; }
+        git clone https://github.com/nimble-technology/nimble-miner-public.git > /dev/null || { echo -e "${red}Failed to clone repository. Returning to options menu.${clear}"; sleep 3; return; }
+        cd nimble-miner-public || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
+        git pull || { echo -e "${red}Failed to pull changes. Returning to options menu.${clear}"; sleep 3; return; }
+        make install || { echo -e "${red}Failed to install miner. Returning to options menu.${clear}"; sleep 3; return; }
     fi
 
     # Ask user to enter a sub address to mine to
@@ -104,15 +117,15 @@ install_miner() {
 }
 
 # Function to run the wallet
-
 run_wallet() {
     if [ ! -f "$HOME/go/bin/nimble-networkd" ]; then
         echo -e "${red}Please install the Wallet first!${clear}"
         sleep 3
+        return
     else
         echo -e "${green}Running the wallet...${clear}"
         sleep 3
-        cd "$HOME/go/bin" || exit
+        cd "$HOME/go/bin" || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
         echo -e "${green}Please enter a name for your wallet...${clear}"
         read -r varwallet
         ./nimble-networkd keys add "$varwallet"
@@ -125,24 +138,25 @@ run_wallet() {
             sleep 3
         else
             echo -e "${red}Please record your wallet seed phrase and address before continuing.${clear}"
-            exit 1
+            sleep 3
+            return
         fi
     fi
 }
 
 # Function to run the miner
-
 run_miner() {
     if [ ! -f "$HOME/nimble/nimble-miner-public/execute.py" ]; then
         echo -e "${red}Please install the Miner first!${clear}"
         sleep 3
+        return
     else
         echo -e "${green}Please enter a sub address to mine to....:${clear}"
         read -r varaddress
-        cd "$HOME/nimble/nimble-miner-public" || exit
+        cd "$HOME/nimble/nimble-miner-public" || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
         # Activate and Start the miner
-        source ./nimenv_localminers/bin/activate
-        tmux new-session -d -s Nimble "make run addr=$varaddress"
+        source ./nimenv_localminers/bin/activate || { echo -e "${red}Failed to activate environment. Returning to options menu.${clear}"; sleep 3; return; }
+        tmux new-session -d -s Nimble "make run addr=$varaddress" || { echo -e "${red}Failed to start miner. Returning to options menu.${clear}"; sleep 3; return; }
         echo $'\r'
         echo -e "${green}Miner has been started in a new Tmux window. Use the command tmux attach to view mining progress${clear}"
         echo $'\r'
@@ -156,13 +170,12 @@ run_miner() {
 }
 
 # Function to check Master Wallet Balance
-
 check_balance() {
     echo -e "${green}Please enter your Master Wallet Address:${clear}"
     read -r varmaster
-    cd "$HOME/nimble/nimble-miner-public" || exit
+    cd "$HOME/nimble/nimble-miner-public" || { echo -e "${red}Failed to change directory. Returning to options menu.${clear}"; sleep 3; return; }
     echo -e "${green}Your Current Nimble Balance is:${clear}"
-    make check addr="$varmaster"
+    make check addr="$varmaster" || { echo -e "${red}Failed to check balance. Returning to options menu.${clear}"; sleep 3; return; }
     echo -e "${green}Press any key to return to the options menu...${clear}"
     read -n 1 -s -r -p "" # Wait for a single keypress
     # Return to the options menu
